4475bb6179a307b9f27d9aee8057f890c7f46dba
Nika <nikataqtaqishvili72@gmail.com>
2026-05-05 01:16:01 +0400

    fix: update frontend deploy and CORS settings

--- app/main.py
+++ app/main.py
@@ -11,6 +11,8 @@ origins = [
     "http://localhost",
     "http://127.0.0.1:5500",
     "http://localhost:5500",
+    "https://taqtaq.github.io",
+    "https://taqtaq.github.io/weather-api-fastapi"
 ]
 app.add_middleware(
     CORSMiddleware,
